0b9ae8eae8ad4498aadfe6097b8111ac23f22a40
QihangDev <qihangdev@gmail.com>
2024-06-27 15:24:46 +0200

    created Board class

--- src/js/Board.js
+++ src/js/Board.js
@@ -1,2 +1,21 @@
 import { Cell } from './Cell.js';
 
+export class Board {
+    constructor(x,y){
+        this.cells = new Map();
+        CreateBoard(x,y);
+    }
+
+    CreateBoard(x,y){
+        for (let i = 0; i < x; i++) {
+            for (let j = 0; j < y; j++) {
+                this.cells.set(`${i},${j}`, new Cell(i,j));
+            }
+        }
+    }
+
+    SelectCell(x,y,value){
+        this.cells.get(`${x},${y}`).Show();
+        this.cells.get(`${x},${y}`).Select(value);
+    }
+}
